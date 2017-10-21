362b64a3a20798422a72b3f81ecc6de0e0664cdb
Dan Tanham <2773809+dtanham@users.noreply.github.com>
2017-10-21 13:34:24 +0100

    Added add document test

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -14,4 +14,8 @@ def test_document_retrieval():
 def test_add_document():
 	u = User.query.first()
 	assert u.id == 1
+	with open("tests/testdata/test_file_1.html", "rb") as f:
+		d = add_document(file_handle=f, title="Test title", description="Small description")
 
+	assert len(get_all_documents()) == 2
+	Document.query.filter(Document.id==d.id).delete()
