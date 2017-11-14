eff5f2724ea83c3f8c8ef17dba906dcf791b299d
David Morris <gm3dmo@gmail.com>
2017-11-14 05:29:42 +0000

    Fixed SQLALCHEMY_TRACK_MODIFICATIONS deprecation warning

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ else:
 
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///test_db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
