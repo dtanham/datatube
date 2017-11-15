760e6830963b21b3083074326719f06e3fe6a5a9
David Morris <gm3dmo@gmail.com>
2017-11-15 11:31:02 +0000

    Fix #16 place a limit on the size of file uploaded
    Why? To protect me from myself and other bad people.

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ else:
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///test_db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAX_CONTENT_LENGTH'] = (16 * 1024) * 1024
 
 db = SQLAlchemy(app)
 
